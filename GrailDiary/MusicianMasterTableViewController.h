0c17711474abb48c5e8eac029de272ec823adb8a
wiseguy16 <gergusa04@gmail.com>
2016-08-01 14:21:51 -0400

    Initial Blank Tableview

--- GrailDiary/MusicianMasterTableViewController.h
+++ GrailDiary/MusicianMasterTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MusicianMasterTableViewController.h
 //  GrailDiary
 //
 //  Created by Gregory Weiss on 8/1/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MusicianMasterTableViewController : UITableViewController
 
 @end
-
